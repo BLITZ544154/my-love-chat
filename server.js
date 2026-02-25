08511593863744767b4b03fbc87f6e80aec9c167
BLITZ544154 <stunchou493@gmail.com>
2026-02-25 11:58:34 +0630

    update login  / REGISTER phone number

--- server.js
+++ server.js
@@ -5,7 +5,6 @@ const port = process.env.PORT || 10000;
 
 app.use(express.static(path.join(__dirname, 'public')));
 
-// စာမျက်နှာ လမ်းကြောင်းများ
 app.get('/', (req, res) => res.sendFile(path.join(__dirname, 'public', 'index.html')));
 app.get('/dashboard', (req, res) => res.sendFile(path.join(__dirname, 'public', 'dashboard.html')));
 app.get('/settings', (req, res) => res.sendFile(path.join(__dirname, 'public', 'settings.html')));
@@ -14,7 +13,6 @@ app.get('/appearance', (req, res) => res.sendFile(path.join(__dirname, 'public',
 app.get('/security', (req, res) => res.sendFile(path.join(__dirname, 'public', 'security.html')));
 app.get('/about-device', (req, res) => res.sendFile(path.join(__dirname, 'public', 'about-device.html')));
 
-// 404 Redirect to Login
 app.use((req, res) => res.sendFile(path.join(__dirname, 'public', 'index.html')));
 
-app.listen(port, () => console.log(`Server started on port ${port}`));
+app.listen(port, () => console.log(`Z-SPACE Engine Active on ${port}`));
